0ad3f5d4c542cb18c584c7bd3ea4e84743e7519d
Alvaro Duran Tovar <alvaro@quiqup.com>
2022-08-08 11:45:05 +0200

    things

---
+++ utils.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+from scipy.stats.distributions import bernoulli
+import pandas as pd
+
+def logit(p):
+    return np.log(p) - np.log(1 - p)
+
+def inv_logit(p):
+    return np.exp(p) / (1 + np.exp(p))
+
+def sim_happiness(seed=1977 , N_years=1000 , max_age=65 , N_births=20 , aom=18):
+    random.seed(seed)
+    H, M, A = np.array([]), np.array([]), np.array([])
+    for t in range(N_years):
+        A += 1
+        A = np.concatenate((A, np.ones(N_births)))
+        H = np.concatenate((H, np.linspace(-2, 2, num=N_births)))
+        M = np.concatenate((M, np.zeros(N_births)))
+
+        for i in range(len(A)):
+            if A[i] >= aom and M[i] == 0:
+                M[i] = bernoulli.rvs(inv_logit(H[i]-4), size=1)
+        
+        deaths = A > max_age
+        if len(deaths) > 0:
+            A = A[~deaths]
+            H = H[~deaths]
+            M = M[~deaths]
+                
+    return pd.DataFrame({"age": A, "happiness": H, "married": M})
